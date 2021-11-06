ac1c548bf5d8938ea4effa954c3fcbe9ca1bd5f0
MrBrrr <d.trojanowsk@gmail.com>
2021-11-06 17:18:09 +0100

    basic solution works

--- src/excel_uploader/views.py
+++ src/excel_uploader/views.py
@@ -15,17 +15,25 @@ class UploaderView(ViewSet):
     def create(self, request, format=None):
         file_uploaded = request.FILES.get("upload_file")
         column_names = request.data.get("column_names")
-        summary = self._summarize_chosen_columns(excel_file=file_uploaded, columns=column_names.split(","))
+        summary = self._summarize_excel_chosen_columns(excel_file=file_uploaded, columns=column_names)
         response = {"filename": file_uploaded.name, "summary": summary}
         return Response(response)
 
-    def _summarize_chosen_columns(self, excel_file, columns):
+    @staticmethod
+    def _summarize_excel_chosen_columns(excel_file, columns):
+
+        # excel file to dataframe
         excel_file.seek(0)
-        content_df = pd.read_excel(excel_file.read())
+        content_df = pd.read_excel(excel_file.read())  # todo jakiś generator żeby nie czytać całego pliku na raz
+
+        # adjust data frame
         content_df.columns = content_df.iloc[1]
         content_df = content_df[2:].reset_index(drop=True)
+        chosen = content_df.loc[2:, [column.strip() for column in columns.split(",")]]
 
-        return [
-            {"column": column, "avg": content_df[column].mean(), "sum": content_df[column].sum()}
-            for column in columns
-        ]
+        # create summary
+        summary = [
+            dict(zip(("column", "avg", "sum"), row))
+            for row in zip(chosen.columns, chosen.mean(), chosen.sum())]
+
+        return summary
